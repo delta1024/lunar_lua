24b7466ff5b08ce75a410652c6c982edc9868f8c
Jake Stannix <jakestannix@gmail.com>
2024-02-01 20:15:58 -0700

    Add rust functaion example

---
+++ examples/rust_fuctions.rs
@@ -0,0 +1,23 @@
+use std::process::exit;
+
+use lunar_lua::{ffi::lua_State, LuaStatePtr, LuaAuxLib, LuaCore, State};
+
+extern "C" fn l_add_two(state: *mut lua_State) -> i32 {
+    let state = LuaStatePtr::from(state);
+    let n = state.aux_check_number(1);
+    state.push(n + 2.0);
+    1
+}
+
+fn main() {
+    let lua = State::new();
+    lua.aux_open_libs();
+    lua.push_c_function(Some(l_add_two));
+    lua.set_global("addtwo");
+    let src = "print(addtwo(3))";
+    if lua.aux_load_buffer(src, "c_func").is_err() || lua.pcall(0, 0, 0).is_err() {
+        let message = lua.to_string(-1);
+        eprintln!("{}", message);
+        exit(1);
+    }
+}
